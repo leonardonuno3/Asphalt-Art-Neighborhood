29f3cd45ebd4491bbd924be7120317af35dff56b
leonardonuno3 <leonardonuno3@gmail.com>
2026-09-14 10:11:45 -0700

    Create OtherEye.java

---
+++ OtherEye.java
@@ -0,0 +1,34 @@
+import org.code.neighborhood.*;
+
+public class OtherEye extends PainterPlus {
+
+    
+    public OtherEye() {
+
+    }
+
+
+    public void paintEye(String color) {
+            moveTwice();
+     moveTwice();
+     moveTwice();
+move();
+      turnRight();
+     moveTwice();
+    paint(color);
+     move();
+     paint(color);
+  move();
+     paint(color);
+            turnRight();
+paint(color);
+  move();
+           paint(color);
+                  turnRight();
+move();
+
+  paint("Black");
+       move();
+           paint(color);
+    }
+}
